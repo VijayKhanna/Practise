7ce187ab994861c11a16f97f440a22a99ca8dd80
mylaptop <mylaptop@192.168.0.9>
2020-06-11 12:59:45 +0530

    Practise 4

---
+++ Palindrome/src/Palindrome.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Palindrome {
+public static void main(String args[])
+{
+	 int r,sum=0,n;    
+	 Scanner sc=new Scanner(System.in);
+	 int num;
+	  num=sc.nextInt();
+	  n=num;    
+	  while(n>0)
+	  {    
+	   r=n%10; 
+	   sum=(sum*10)+r;    
+	   n=n/10;    
+	  }    
+	  if(num==sum)    
+	   System.out.println("palindrome number ");    
+	  else    
+	   System.out.println("not palindrome"); 
+}
+}
