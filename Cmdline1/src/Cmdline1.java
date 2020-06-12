64d8dc382d27818a481318ddf712afbf2d18c1e3
mylaptop <mylaptop@192.168.0.9>
2020-06-12 07:01:05 +0530

    Practise 5

---
+++ Cmdline1/src/Cmdline1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Cmdline1 
+{
+	public static void main(String args[])
+	{
+		String str1,str2;
+		Scanner sc=new Scanner(System.in);
+		str1=sc.nextLine();
+		str2=sc.nextLine();
+		System.out.print(str1);
+		System.out.print(" Technologies ");
+		System.out.print(str2);
+	}
+}
