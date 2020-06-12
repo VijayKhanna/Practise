1c17a9d75966b52878977e0bf6ded33466a332fe
mylaptop <mylaptop@192.168.0.9>
2020-06-12 07:06:07 +0530

    Practise 6

---
+++ Cmdline2/src/Cmdline2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Cmdline2 
+{
+	public static void main(String args[])
+	{
+		String str1,str2;
+		Scanner sc=new Scanner(System.in);
+		str1=sc.nextLine();
+		str2=sc.nextLine();
+		System.out.print(str1);
+		System.out.print(" Technologies ");
+		System.out.print(str2);
+	}
+
+}
